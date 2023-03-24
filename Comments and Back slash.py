c27e06bae187f112480b7dac76fa10bc028b76c3
Kuldeep Malviya <79964548+RedxNooB@users.noreply.github.com>
2023-03-24 16:58:28 +0530

    Add files via upload

---
+++ Comments and Back slash.py
@@ -0,0 +1,9 @@
+print("Hello World")
+#This is a comment so ignore it.
+print("Now, this is a message again\nAnd also, \"space\"")
+# And a comment again
+# another comment
+'''This is an another way to wright
+a comment using Double or single court'''
+print("Hello", 1, 2, sep="~", end="Epic\n")
+print("Kuldeep")
