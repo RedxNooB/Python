7610dc41887af50a5afcdc3c51b3db1472761db8
Kuldeep Malviya <79964548+RedxNooB@users.noreply.github.com>
2023-03-25 22:43:28 +0530

    Add files via upload

---
+++ Variables and data types.py
@@ -0,0 +1,20 @@
+#Variables are like containers in kitchen, which are used to store items, just like that creating variables is like assinging it\nsome data.
+a = 9977
+b = "BajungaChan"
+c = 123
+d = "Kuldeep"
+e = True
+f = 1.9566
+g = complex(1, 5)
+dict1 = {"name":"Kuldeep", "age":20}
+print(a, b)
+print(a + c)
+print(b + d)
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of e is", type(e))
+print("The type of f is", type(f))
+print("The type of g is", type(g))
+print(dict1)
+print("The type of dict1 is", type(dict1))
+#Every thing in python is an object.
